b2bbea99f2b283e5a9b579c7341b0cc797f0b4dd
Alex <alejandro.deandres2@gmail.com>
2024-06-06 21:16:56 +0200

    Segundo día

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    /** Hamburger menu displayer */
+    sidebar: boolean = false;
 
+    /** Method to activating / deactivating hamburger menu */
+    changeSidebar() {
+        this.sidebar = !this.sidebar;
+    }
 }
